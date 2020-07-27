660ce2bef5243f7a1394932ae7bcf00630b0c5b9
jnti <johnim2247@gmail.com>
2020-07-27 18:00:01 -0400

    Added dotenv variables

--- express-api/queries.js
+++ express-api/queries.js
@@ -1,10 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: "postgres",
-    host: "127.0.0.1",
-    database: "postgres",
-    password: "password",
-    port: 5432
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 })
 
 const getDates = (request, response) => {
